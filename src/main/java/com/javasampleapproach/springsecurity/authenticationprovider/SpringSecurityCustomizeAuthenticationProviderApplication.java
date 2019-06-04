8deabf8f8c2bc9423d47c36d4fea7c0c9e82e8fd
Your Name <artaud1@gmail.com>
2019-06-04 23:43:22 +0200

    spring security with spring thymeleaf

--- src/main/java/com/javasampleapproach/springsecurity/authenticationprovider/SpringSecurityCustomizeAuthenticationProviderApplication.java
+++ src/main/java/com/javasampleapproach/springsecurity/authenticationprovider/SpringSecurityCustomizeAuthenticationProviderApplication.java
@@ -1,12 +1,33 @@
 package com.javasampleapproach.springsecurity.authenticationprovider;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class SpringSecurityCustomizeAuthenticationProviderApplication {
+public class SpringSecurityCustomizeAuthenticationProviderApplication implements CommandLineRunner {
+	@Value( "${jdbc.url}" )
+	private String jdbcUrl;
+	
+	@Autowired
+	private ApplicationContext appContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityCustomizeAuthenticationProviderApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String[] beans = appContext.getBeanDefinitionNames();
+		Arrays.sort(beans);
+		for (String bean : beans) {
+			System.out.println(bean);
+		}		
+		System.out.println(jdbcUrl);
 	}
 }
