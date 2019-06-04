845d88b90df2be1a808c148c1b8964fd318dcf28
Your Name <artaud1@gmail.com>
2019-06-04 23:16:22 +0200

    spring security with spring thymeleaf

---
+++ src/main/java/com/javasampleapproach/springsecurity/authenticationprovider/SpringSecurityCustomizeAuthenticationProviderApplication.java
@@ -0,0 +1,12 @@
+package com.javasampleapproach.springsecurity.authenticationprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCustomizeAuthenticationProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCustomizeAuthenticationProviderApplication.class, args);
+	}
+}
